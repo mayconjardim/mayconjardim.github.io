dbf7df72236dc6b8ced1c0e978e65f703c249666
Maycon Jardim <mayconsmill@gmail.com>
2023-01-05 16:17:37 -0300

    Componente projetos html, css e ts

--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -1,3 +1,4 @@
+import { Projetos } from './../../models/projetos';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projetos.component.scss'],
 })
 export class ProjetosComponent implements OnInit {
+  projetos: Projetos[] = [
+    {
+      titulo: 'NBALiga Basketball',
+      tecnologias: 'Spring, MySQL, Angular, Material e Bootstrap',
+      img: 'assets/projetos/nbaliga2.png',
+      repo: 'https://github.com/mayconjardim/nbaliga-frontweb',
+      link: 'https://nbaliga-9b2b8.web.app/',
+      descricao: [
+        'NBALiga é uma liga de simulação da NBA, Os membros da liga assumem o papel de' +
+          'GM e podem fazer trocas, assinar jogador e outras utilidades na aplicação',
+      ],
+    },
+    {
+      titulo: 'Wallace Building Products',
+      tecnologias: 'Spring, PostgreSQL, Angular e Boostrap',
+      img: 'assets/projetos/wallacebp.png',
+      descricao: [
+        'Aplicação para gerenciamento de ordems de serviços, com uma interface prática os usuarios da' +
+          'empresa conseguem gerenciar e acompanhar a movimentação de pedidos',
+      ],
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/models/projetos.ts
@@ -0,0 +1,8 @@
+export interface Projetos {
+  titulo: string;
+  tecnologias: string;
+  img: string;
+  descricao: string[];
+  link?: string;
+  repo?: string;
+}
