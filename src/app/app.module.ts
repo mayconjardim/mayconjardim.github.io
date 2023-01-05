aeb2fd02712d105e8768606301a429eaf5262396
Maycon Jardim <mayconsmill@gmail.com>
2023-01-05 14:01:50 -0300

    Componente projetos criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { InfoPessoalComponent } from './components/info-pessoal/info-pessoal.com
 import { EducacaoComponent } from './components/educacao/educacao.component';
 import { ExpProfissionalComponent } from './components/exp-profissional/exp-profissional.component';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HabilidadesComponent } from './components/habilidades/habilidades.compo
     InfoPessoalComponent,
     EducacaoComponent,
     ExpProfissionalComponent,
-    HabilidadesComponent
+    HabilidadesComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule
